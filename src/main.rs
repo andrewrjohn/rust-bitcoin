9fcbebc3e90d87fc91a9c90e3ba98b89e15bf0b5
Andrew Johnson <johnsonrobertandrew@gmail.com>
2021-11-11 09:51:08 -0600

    invalid menmonic

--- src/main.rs
+++ src/main.rs
@@ -74,6 +74,81 @@ impl Seed {
                     all_words.push(word);
                 }
                 let mut seed: Vec<String> = vec![];
+                let mut bits = String::new();
+
+                for _ in 0..256 {
+                    let mut rng = rand::thread_rng();
+                    let bit = rng.gen_range(0..=1);
+
+                    bits.push_str(&bit.to_string());
+                }
+
+                println!("{}", bits);
+
+                // Checksum
+                let hex = sha256_digest(&bits);
+                let first = hex.chars().nth(0).unwrap();
+                let second = hex.chars().nth(1).unwrap();
+
+                let decimal_map: HashMap<char, i32> = HashMap::from([
+                    ('0', 0),
+                    ('1', 1),
+                    ('2', 2),
+                    ('3', 3),
+                    ('4', 4),
+                    ('5', 5),
+                    ('6', 6),
+                    ('7', 7),
+                    ('8', 8),
+                    ('9', 9),
+                    ('A', 10),
+                    ('B', 11),
+                    ('C', 12),
+                    ('D', 13),
+                    ('E', 14),
+                    ('F', 15),
+                ]);
+
+                if let Some(dec) = decimal_map.get(&first) {
+                    let binary = format!("{:0>4}", format!("{:b}", dec));
+                    bits.push_str(&binary);
+                }
+
+                if let Some(dec) = decimal_map.get(&second) {
+                    let binary = format!("{:0>4}", format!("{:b}", dec));
+
+                    bits.push_str(&binary);
+                }
+
+                println!("{}", bits.chars().count());
+
+                let mut binary_groups: Vec<String> = vec![];
+
+                // We want 24 groups of 11 digit binary strings
+                for _ in 0..24 {
+                    let group = &bits[0..11];
+                    binary_groups.push(group.to_string());
+                    bits.replace_range(0..11, "");
+                }
+
+                let mut decimal_groups: Vec<usize> = vec![];
+
+                for binary in binary_groups {
+                    let decimal = usize::from_str_radix(&binary, 2).unwrap();
+
+                    decimal_groups.push(decimal);
+                }
+                println!("{:?}", decimal_groups);
+
+                let mut words: Vec<String> = vec![];
+
+                for decimal in decimal_groups {
+                    words.push(all_words[decimal].to_string());
+                }
+
+                let words = words.join(" ");
+                println!("{:?}", words);
+
                 for _ in 0..11 {
                     let mut rng = rand::thread_rng();
                     let random_number = rng.gen_range(0..2048);
@@ -83,7 +158,6 @@ impl Seed {
                 let seed = seed.join(" ");
                 // println!("{:?}", seed);
 
-                let binary = format!("{:b}", seed.replace(" ", ""));
                 return Some(seed);
             } else {
                 println!("Unable to open wordlist file");
@@ -108,7 +182,7 @@ impl Seed {
 }
 
 fn main() {
-    let rand_priv_key = PrivKey::from_random();
+    // let rand_priv_key = PrivKey::from_random();
     let seed = Seed::from_random();
 
     // let priv_key = PrivKey::from_seed_phrase(seed.mnemonic);
@@ -116,7 +190,7 @@ fn main() {
     // println!("{}", rand_priv_key.decimal);
     // println!("{}", priv_key.decimal);
     // println!("{}", seed.hex);
-    println!("{}", seed.mnemonic.split(" ").count());
+    // println!("{}", seed.mnemonic.split(" ").count());
 }
 
 // #[cfg(test)]
