cc4ed61b54f79dd2de57582e3d9c485a8b8bfa73
Andrew Johnson <johnsonrobertandrew@gmail.com>
2021-12-01 17:02:42 -0600

    working encryption/decryption

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,45 @@
-use bs58;
-use data_encoding::{HEXLOWER, HEXUPPER};
-use hex::FromHex;
-use num_bigint::{BigInt, Sign};
-use rand::{thread_rng, CryptoRng, Rng};
-// extern crate ring::digest::{Context, Digest, SHA256};
-// use ring::rand::SecureRandom;
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::str;
-use sv::util::sha256d;
-use sv::wallet::{load_wordlist, mnemonic_decode, mnemonic_encode, Wordlist};
+use bsv_wasm::{ECIESCiphertext, PrivateKey, ToHex};
 
-fn sha256_digest(data: &str) -> String {
-    let hash_hex = sha256d(data.as_bytes()).encode();
+// Convert to binary/buffer
+// JS - Buffer.from("my string")
+// Rust - "my string".as_bytes()
 
-    // context.update(data.as_bytes());
-
-    // HEXUPPER.encode(context.finish().as_ref())
-    return hash_hex;
-}
-
-struct Utils {}
-
-impl Utils {
-    fn generate_words() -> Option<String> {
-        let bits = generate_entropy();
-        let bits: &[u8] = &bits;
-
-        let mnemonic = mnemonic_encode(bits, &load_wordlist(Wordlist::English));
-        let mnemonic_string = mnemonic.join(" ");
-
-        return Some(mnemonic_string);
-    }
-}
-
-struct PrivKey {
-    decimal: BigInt,
-    seed: String,
-}
-
-impl PrivKey {
-    fn from_random() -> Self {
-        let seed = Seed::from_random();
-        let seed = seed.mnemonic;
-
-        let hash = sha256_digest(&seed);
-        let decimal = BigInt::parse_bytes(hash.as_bytes(), 16).unwrap();
-
-        Self { decimal, seed }
-    }
-
-    fn from_seed_phrase(seed: String) -> Self {
-        if seed.split(" ").count() != 12 {
-            panic!("Seed phrase must be 12 words.")
-        }
-
-        let hash = sha256_digest(&seed);
-        let decimal = BigInt::parse_bytes(hash.as_bytes(), 16).unwrap();
-
-        Self { decimal, seed }
-    }
-
-    fn to_wif(&self) -> String {
-        format!(
-            "{}{}",
-            "K",
-            bs58::encode(&self.decimal.to_str_radix(16)).into_string()
-        )
-        .to_string()
-    }
-}
-
-fn generate_entropy() -> Vec<u8> {
-    let mut bits: Vec<u8> = vec![];
-
-    for _ in 0..32 {
-        let mut rng = rand::thread_rng();
-        let bit = rng.gen_range(0..=128);
-
-        bits.push(bit.clone());
-    }
-
-    return bits;
-}
-
-struct Seed {
-    mnemonic: String,
-    hex: String,
-}
-
-impl Seed {
-    fn from_random() -> Self {
-        match Utils::generate_words() {
-            Some(words) => {
-                let hex = hex::encode(&words);
-                Seed {
-                    mnemonic: words,
-                    hex,
-                }
-            }
-            None => panic!("Cannot create seed"),
-        }
-    }
-
-    // fn from_mnemonic(mnemonic: String) -> Self {
-    //     let words = men
-    // }
+fn bytes_to_str(bytes: Vec<u8>) -> String {
+    return std::str::from_utf8(&bytes[..]).unwrap().to_string();
 }
 
 fn main() {
-    // let rand_priv_key = PrivKey::from_random();
-    let seed = Seed::from_random();
+    println!("{:?}", "hello".as_bytes());
+    let priv_key = PrivateKey::from_random();
+    let pub_key = priv_key.to_public_key().unwrap();
+    let wif = priv_key.to_wif().unwrap();
 
-    println!("{:?}", seed.mnemonic);
-    // let priv_key = PrivKey::from_seed_phrase(seed.mnemonic);
+    //  Encryption
+    let secret_text = "hello world my name in Andrew";
+    let encrypted_message = priv_key
+        .encrypt_message(secret_text.as_bytes())
+        .expect(format!("Can't encrypt message: {}", secret_text).as_str())
+        .to_bytes()
+        .to_hex();
+    println!("Encrypted: {}", encrypted_message);
 
-    // println!("{}", rand_priv_key.decimal);
-    // println!("{}", priv_key.decimal);
-    // println!("{}", seed.hex);
-    // println!("{}", seed.mnemonic.split(" ").count());
+    //
+    // ------------------------------
+    //
+
+    // Decryption
+    let new_priv_key = PrivateKey::from_wif(wif.as_str())
+        .expect("Can't create Private Key from provided WIF string");
+
+    let decoded_encrypted_messaged =
+        &hex::decode(encrypted_message).expect("Can't decode encrypted message from string")[..];
+    let new_cipher = ECIESCiphertext::from_bytes(decoded_encrypted_messaged, true)
+        .expect("Can't create new cipher");
+
+    let decrypted_message =
+        bytes_to_str(new_priv_key.decrypt_message(&new_cipher, &pub_key).unwrap());
+
+    println!("1st wif: {}", wif);
+    println!("2nd wif: {}", new_priv_key.to_wif().unwrap());
+    println!("Decrypted: {}", decrypted_message);
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn priv_key_from_seed_phrase() {
-//         let seed = Seed::from_random();
-//         let priv_key = PrivKey::from_seed_phrase(seed.mnemonic);
-
-//         assert_eq!(seed.mnemonic, priv_key.seed.to_string());
-//     }
-// }
